d54a6e610015508463e365792ed3fc02813d175b
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-12-25 18:08:24 +0530

    Implement email verification logic and enhance error handling in user verification route

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,18 +1,40 @@
-import { connectDB } from "@/dbConfig/dbConfig";
-import { NextRequest, NextResponse } from "next/server";
-import User from "@/models/userModel";
+import { connectDB } from '@/dbConfig/dbConfig';
+import { NextRequest, NextResponse } from 'next/server';
+import User from '@/models/userModel';
 
 connectDB();
 
-export async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
   try {
     const reqbody = await request.json();
-    const {token} = reqbody;
-    console.log('token',token);
+    const { token } = reqbody;
+    console.log('token', token);
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() }
+    });
+    if (!user) {
+      return NextResponse.json(
+        { error: 'Invalid or expired token' },
+        {
+          status: 400
+        }
+      );
+    }
+    console.log(user);
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+    return NextResponse.json(
+      { message: 'Email verified successfully', success: true },
+      {
+        status: 200
+      }
+    );
   } catch (error: unknown) {
-     const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-     return NextResponse.json({error: errorMessage},
-      {status: 500}
-     )
+    const errorMessage =
+      error instanceof Error ? error.message : 'Unknown error';
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
   try {
+    console.log('hi');
     await mongoose.connect(process.env.MONGO_URI!);
     const connection = mongoose.connection;
     connection.on('connected', () => {
